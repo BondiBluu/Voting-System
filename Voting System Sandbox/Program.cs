c9df3b35ba2c5db8020f47bbe23aadf643629e5d
Bami O. <bamideleogundeji949@gmail.com>
2024-08-19 11:29:35 -0400

    Add project files.

---
+++ Voting System Sandbox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voting_System_Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yesCounter = 0;
+            int noCounter = 0;
+
+            Console.WriteLine("Welcome to the voting system- Vote yes (y) or no (n).");
+            string vote = Console.ReadLine();
+
+            if(vote == "y")
+            {
+                yesCounter++;
+            }
+            else if (vote == "n")
+            {
+                noCounter++;
+            }
+            else
+            {
+                Console.WriteLine("Invalid vote.");
+            }
+
+            Console.WriteLine("The results are in: ");
+            Console.WriteLine("Yes: " + yesCounter);
+            Console.WriteLine("No: " + noCounter);
+
+            VotingResults(yesCounter, noCounter);
+
+            Console.ReadLine();
+        }
+
+        static void VotingResults(int yesCounter, int noCounter)
+        {
+            if (yesCounter > noCounter)
+            {
+                Console.WriteLine("The majority voted yes.");
+            }
+            else if (yesCounter < noCounter)
+            {
+                Console.WriteLine("The majority voted no.");
+            }
+            else
+            {
+                Console.WriteLine("Draw.");
+            }
+        }
+    }
+}
